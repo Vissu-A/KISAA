985394ba49619839eff7b2de82bac112d2f4675c
Vissu-A <viswanadh.adapala.org@gmail.com>
2024-03-08 19:36:32 +0530

    new changes

--- kisaa/urls.py
+++ kisaa/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/user/', include("userapi.urls")),
-]
+]+ static(settings.STATIC_URL)
